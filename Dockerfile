62df0c76dae1d17540d9a24a9fc2b9189033230a
Infanta Xavier <xavier8338@yahoo.com>
2015-08-08 21:33:43 +0530

    made parameters as build environments

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
 from ubuntu:latest
 
+ENV PUBLIC_IP
+ENV INTERNAL_IP
+ENV PUBLIC_DOMAIN
 RUN apt-get update
 RUN apt-get install -y iodine iptables iptables-persistent
 COPY firerules.txt /etc/iptables/rules.v4
 COPY entrypoint.sh /
 
-CMD ["/bin/bash","/entrypoint.sh"]
+CMD ["/bin/bash","/entrypoint.sh",{PUBLIC_IP},{INTERNAL_IP},{PUBLIC_DOMAIN}]
 
 EXPOSE 53
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Hello World"
+PUBLIC_IP=$1
+INTERNAL_IP=$2
+PUBLIC_DOMAIN=$3
 /sbin/iptables-restore < /etc/iptables/rules.v4
-/usr/sbin/iodined -f -c -P i0d1n3 -n $PUBLIC_IP -F /var/run/iodine/iodine.pid 192.168.108.13 $PUBLIC_DOMAINNAME
+/usr/sbin/iodined -f -c -P i0d1n3 -n $PUBLIC_IP -F /var/run/iodine/iodine.pid $INTERNAL_IP $PUBLIC_DOMAIN
 
